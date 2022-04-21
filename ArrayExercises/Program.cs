6753bb7b6570d4dd19d4264391390af3def1a1a0
effdiggity <dear.francesca@gmail.com>
2022-04-21 14:21:18 -0400

    sone with strings and array work

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -5,8 +5,21 @@ namespace ArrayExercises
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Arrays!");
+        { 
+int[] intArray = {1, 1, 2, 3, 5, 8};
+            Console.WriteLine(intArray);
+            //for (int i = 0; i < intArray.Length; i++)
+            //{
+            //    Console.WriteLine(intArray[i]);
+            //}
+
+                foreach (int integer in intArray)
+            {
+                if (integer % 2 != 0) {  //or could do %2 == 1
+                       Console.WriteLine(integer);
+    
+                }
+            }
         }
     }
 }
