5402da08e354fc819c081910297a64d574c52bc7
effdiggity <dear.francesca@gmail.com>
2022-04-21 15:26:30 -0400

    list work done

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -6,7 +6,28 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lists!");
+            //create list w/ 10+ ints
+            //new keyword creates and empty instance of List class (ie an eplty array [])
+            List<int> integerArray = new List<int>(); //usu create an empty list b/c the data get filled in later with fetched data, use input, etc
+            //but could populate the list with {item1, item2, etc} if already have the data
+
+
+
+            //Console.WriteLine("Hello Lists!");
+        }
+        //wrtie statis list method to find sum of EVEN nums in list
+        //input: list of ints
+        //output: sim of all EVEN nums
+        static int AddEvensOnly(List<int> integerList) //methods use pasclase case, hence AddEvensOnly
+        { //method, working as a fxn, inside of a class
+foreach (int integer in integerList)
+            {
+                if (integer % 2 == 0)
+                {
+                    sum += integer;
+                }
+            }
+
         }
     }
 }
